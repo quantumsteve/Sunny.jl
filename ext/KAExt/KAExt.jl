29ea8b54037bb30d6789a720ca5a7a844672d344
Steven Hahn <hahnse@ornl.gov>
2026-09-08 11:00:04 -0400

    fix test failure?
    Signed-off-by: Steven Hahn <hahnse@ornl.gov>

--- ext/KAExt/KAExt.jl
+++ ext/KAExt/KAExt.jl
@@ -2,8 +2,12 @@ module KAExt
 
 using Adapt: Adapt
 using KernelAbstractions: KernelAbstractions, @groupsize, @index, @kernel, @localmem, @synchronize, @uniform
+using CUDA: CUDA, @cuda, CUBLAS, CUSOLVER, CuArray, CuDynamicSharedArray,
+            CuMatrix, CuPtr, CuVector, StridedCuArray, blockDim, blockIdx,
+            launch_configuration, threadIdx
 using StaticArrays: StaticArrays, SArray, SMatrix, SVector
-using Sunny: Sunny
+using Sunny: Sunny, Bond, Crystal, SpinWaveTheory, System, global_position, 
+             intensities_bands
 
 include("System/Types.jl")
 include("System/TypesSUN.jl")
