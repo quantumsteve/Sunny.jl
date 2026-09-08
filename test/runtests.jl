1d177ec4f36fd6f1f6d2a4f23f59ea4648ed9304
Steven Hahn <hahnse@ornl.gov>
2026-09-08 14:01:27 -0400

    only run when cuda is available?
    Signed-off-by: Steven Hahn <hahnse@ornl.gov>

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,12 @@
 using TestItemRunner
+using CUDA
 
-@run_package_tests filter=test->!(:skip in test.tags)
+# Determine which tags to target
+if CUDA.functional()
+    @info "CUDA detected. Running full test suite including GPU."
+    @run_package_tests filter=test->!(:skip in test.tags)
+else
+    @warn "CUDA not available. Running CPU tests only."
+    @run_package_tests filter=test -> !(:skip in test.tags) && !(:cuda in test.tags)
+end
 
