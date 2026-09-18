99452dcd12c1e93682a29a59a97f940b97fc14b4
Steven Hahn <hahnse@ornl.gov>
2026-09-18 16:19:48 -0400

    Add CUDA version info to test runner

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,7 @@ using CUDA
 # Determine which tags to target
 if CUDA.functional()
     @info "CUDA detected. Running full test suite including GPU."
+    CUDA.versioninfo()
     @run_package_tests filter=test->!(:skip in test.tags)
 else
     @warn "CUDA not available. Running CPU tests only."
